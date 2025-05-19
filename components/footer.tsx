6e3541fba8fa45a3cea6f773af1332fd2d61b393
Rahul Gupta <rg5353070@gmail.com>
2025-05-19 20:02:03 +0530

    made changes to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -20,10 +20,9 @@ export default function Footer() {
                 <div className="text-sm text-special">Knowledge Partners</div>
               </div>
             </div>
-            <p className="">
-              OneBridge is a trusted knowledge partner in the mid-Market M&A,
-              providing agile and cost effective support to boutique M&A
-              Investment Banks and Advisory firms.
+            <p>
+              Helping M&A Advisory firms and Investment Banks in accelerating
+              delivery to their clients.
             </p>
           </div>
 
@@ -60,13 +59,13 @@ export default function Footer() {
 
           <div className="col-span-1 mx-auto">
             <h3 className="text-special font-semibold mb-4">Contact Us</h3>
-            <p className="flex items-center mb-2">
+            <span className="flex items-center mb-2">
               <Phone size={16} className="mr-2" />
               <Link href="tel:+918561046369" className="hover:underline">
                 +91 85610 46369
               </Link>
-            </p>
-            <p className="flex items-center ">
+            </span>
+            <span className="flex items-center">
               <Mail size={16} className="mr-2" />
               <Link
                 href="mailto:info@onebridge.com"
@@ -74,18 +73,13 @@ export default function Footer() {
               >
                 info@onebridge.com
               </Link>
-            </p>
+            </span>
           </div>
         </div>
 
         {/* Bottom Section */}
         <div className="mt-12 pt-6 border-t border-white/20 flex flex-col lg:flex-row justify-between items-start lg:items-center">
-          <div className="max-w-2xl mb-4 lg:mb-0">
-            <p>
-              Helping M&A Advisory firms and Investment Banks in accelerating
-              delivery to their clients.
-            </p>
-          </div>
+          <div className="max-w-2xl mb-4 lg:mb-0"></div>
           <div>
             <p>Copyright © {currentYear} OneBridge Knowledge Partners</p>
           </div>
