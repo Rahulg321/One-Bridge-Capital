63466ca1cb7082df1f9d5af35fe61975746751e4
Rahul Gupta <rg5353070@gmail.com>
2025-05-27 15:47:05 +0530

    added a gray for engage with us

--- slices/EngageSidebarContent/index.tsx
+++ slices/EngageSidebarContent/index.tsx
@@ -19,36 +19,36 @@ const EngageSidebarContent: FC<EngageSidebarContentProps> = ({ slice }) => {
     {
       title: "Model 1: Block of Hours",
       description:
-        "Client purchases a block of hours that can be used any time during the month",
-      suitable: "Clients with variable workflow; support during peak cycles",
+        "Client purchases a block of hours that can be used any time during the month.",
+      suitable: "Clients with variable workflow; support during peak cycles.",
       highlights: [
-        "Flexibility to utilize capacity at any time during the month",
-        "Teams are staffed with relevant subject-matter experts as per project requirements",
-        "The client will always have a single point of contact at Acuity Knowledge Partners",
+        "Flexibility to utilize capacity at any time during the month.",
+        "Teams are staffed with relevant subject-matter experts as per project requirements.",
+        "The client will always have a single point of contact at Acuity Knowledge Partners.",
       ],
     },
     {
       title: "Model 2: Dedicated Staff Model",
       description:
-        "Team is solely dedicated to the client and works as an extension of the client's onshore team",
+        "Team is solely dedicated to the client and works as an extension of the client's onshore team.",
       suitable:
-        "Seamless team extension; clients with steady workflow, for provision of basic to complex support",
+        "Seamless team extension; clients with steady workflow, for provision of basic to complex support.",
       highlights: [
-        "Fosters a deep understanding of the client's requirements and a strong relationship with the onshore team",
-        "Knowledge sharing leads to high value addition and increased efficiency",
-        "Teams are staffed in a secure single-client room",
+        "Fosters a deep understanding of the client's requirements and a strong relationship with the onshore team.",
+        "Knowledge sharing leads to high value addition and increased efficiency.",
+        "Teams are staffed in a secure single-client room.",
       ],
     },
     {
       title: "Model 3: Ad-hoc Project Model",
       description:
-        "Support on a standalone project basis, with no long-term commitment",
-      suitable: "Standalone tasks and smaller engagements",
+        "Support on a standalone project basis, with no long-term commitment.",
+      suitable: "Standalone tasks and smaller engagements.",
 
       highlights: [
-        "Ability to staff any team size at short notice for any time period",
-        "Teams are staffed with relevant subject-matter experts as per project requirements",
-        "Full visibility of project costing",
+        "Ability to staff any team size at short notice for any time period.",
+        "Teams are staffed with relevant subject-matter experts as per project requirements.",
+        "Full visibility of project costing.",
       ],
     },
   ];
@@ -73,14 +73,20 @@ const EngageSidebarContent: FC<EngageSidebarContentProps> = ({ slice }) => {
                     {model.title}
                   </h4>
                   <span className="mb-4 md:mb-4 block lg:mb-6 text-xs">
-                    <strong>Description:</strong> {model.description}
+                    <strong>Description:</strong>{" "}
+                    <span className="text-xs text-gray-700">
+                      {model.description}
+                    </span>
                   </span>
                   <span className="mb-4 md:mb-4 block lg:mb-6 text-xs">
-                    <strong>Suitable for:</strong> {model.suitable}
+                    <strong>Suitable for:</strong>{" "}
+                    <span className="text-xs text-gray-700">
+                      {model.suitable}
+                    </span>
                   </span>
                   <div className="block">
-                    <strong>Highlight:</strong>
-                    <ul className="list-disc pl-5 mt-1 space-y-2 text-xs">
+                    <strong className="text-xs">Highlight:</strong>
+                    <ul className="list-disc pl-5 mt-1 space-y-2 text-xs text-gray-700">
                       {model.highlights.map((highlight, i) => (
                         <li key={i}>{highlight}</li>
                       ))}
